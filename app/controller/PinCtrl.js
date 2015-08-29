b2177d77035061c4f73c743a6973054ef87e117b
danielcossins11 <daniel.cossins@yahoo.com>
2015-08-29 16:19:09 -0500

    modal popping up on pin button click

--- app/controller/PinCtrl.js
+++ app/controller/PinCtrl.js
@@ -40,11 +40,18 @@ app.controller("PinCtrl",
       var id = angular.element(this).parent()[0].id;
       console.log(id);
       console.log($scope.pins);
+      var pin = {};
 
       angular.forEach($scope.pins, function(value, key) {
         if(value.text.title===id){
-          //push value to the local pins array
-        }        
+          //save the matching value to pin
+          pin = value;
+        }
+
+        //on another button click (final pin button in the modal)
+          //take the text input from modal
+          //and change the description to match the new text
+          //then push the new pin object to the user's array
       });
     });
   }
