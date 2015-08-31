bce2760a986e565a25a3473e662a30bab66f85a6
Sally Fisher <sqfisher@gmail.com>
2015-08-31 13:36:48 -0500

    MVP for creating pin from website URL, still need to add in functionality for uploading and for adding user id along with new pin

--- app/controller/PinCtrl.js
+++ app/controller/PinCtrl.js
@@ -21,16 +21,8 @@ app.controller("PinCtrl",
         placement : 'left',
         trigger : 'click',
         html: true,
-        content: '<a href="#" class="btn btn-block btn-sm btn-default"><span class="glyphicon glyphicon-arrow-up"></span> Upload a pin</a><a data-toggle="modal" id="pinFromWebsite" data-target="#addPinFromWebsite" class="btn btn-block btn-sm btn-default"><span class="glyphicon glyphicon-globe"></span> Pin from website</a>'
+        content: '<a href="#" class="btn btn-block btn-sm btn-default"><span class="glyphicon glyphicon-arrow-up"></span> Upload a pin</a><a href="#/createpin" class="btn btn-block btn-sm btn-default"><span class="glyphicon glyphicon-globe"></span> Pin from website</a>'
       });
-
-  });
-
-
-  $scope.createPin = function() {
-    // $location.url('/createpin');
-    // $("#createPin").tooltip(); 
-
-  };
-}
+    });
+  }
 ]);
